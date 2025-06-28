55738465fa745412e6971f6d226a700c1e19503d
Fern Bromley <fernb@arizona.edu>
2025-06-28 10:45:31 -0700

    continuing temp inquiries...

--- scripts/Provenance_Comparison.R
+++ scripts/Provenance_Comparison.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+options(digits = 10)
+
 
 # DAILY data acquired from PRISM explorer (https://prism.oregonstate.edu/explorer/) on Thursday, June 19th, 2025
 # see acquisition parameters in original CSVs
@@ -39,18 +41,50 @@ names(prov_clim) <- c("date", "tmin", "tmean", "tmax", "vpdmin", "vpdmax",
 #   group_by(year) %>% 
 #   summarise(temp = mean(tmean))
 
+# let's get some z-scores
+
+prov_z_june <- prov_clim %>% 
+  group_by(spp) %>% 
+  filter(month == 6) %>% 
+  summarise(tmax_mean = mean(tmax), tmax_sd = sd(tmax)) %>% 
+  ungroup() %>% 
+  mutate(tmax_lo = tmax_mean - tmax_sd,
+         tmax_hi = tmax_mean + tmax_sd)
+
+ggplot(prov_z_june, aes(x = spp, y = tmax_mean))+
+  geom_point(size = 4)+
+  geom_errorbar(aes(ymin = tmax_lo, ymax = tmax_hi), width = 0.2)+
+  geom_hline(yintercept = mean(prov_z_june$tmax_mean), color = "blue")+
+  labs(x = "Species", y = "Mean max daily temp in June (ºC) ± SD")+
+  theme_light(base_size = 26)
+
+mean(prov_z_june$tmax_mean)
+
+# TukeyHSD(aov(tmax ~ spp, data = prov_clim), conf.level = 0.95)
+
 
 ggplot(prov_clim, aes(x = doy, y = tmax))+
   geom_point(aes(group = year, color = spp), alpha = 0.2)+
   geom_smooth(aes(group = spp, color = spp))
 
-prov_clim <- prov_clim %>% 
-  filter(!is.na(tmax)) %>% 
-  mutate(tmax = as.numeric(tmax))
+prov_clim_z <- prov_clim %>% 
+  filter(month == 6) %>% 
+  full_join(prov_z_june, by = join_by(spp)) %>% 
+  mutate(tmax_z = (tmax - tmax_mean)/tmax_sd) %>% 
+  select(spp, date, year, doy, tmax_z)
+
+ggplot(prov_clim_z, aes(x = doy, y = tmax_z, group = interaction(spp, year)))+
+  geom_line(aes(color = spp), alpha = 0.1)
+
 
 prov_doy_stats <- prov_clim %>% 
   group_by(spp, doy) %>% 
-  summarise(tmax = max(tmax))
+  summarise(tmax_mean = mean(tmax, na.rm = T), tmax_sd = sd(tmax, na.rm = T))
+
+ggplot(na.omit(prov_doy_stats), aes(x = doy, y = tmax_mean, color = spp))+
+  geom_ribbon(aes(ymin = tmax_mean - tmax_sd, ymax = tmax_mean + tmax_sd, fill = spp),
+              alpha = 0.3)+
+  geom_line(linewidth = 1.5)
 
 # ggplot(prov_clim, aes(x = spp, y = tmax))+
 #   geom_boxplot(aes(fill = spp))+
@@ -73,31 +107,24 @@ ggplot(filter(prov_clim, month(date) == 6), aes(x = spp, y = tmax))+
        fill = "Species")+
   theme_light(base_size = 20)
 
-
-
-
-
-ggplot(prov_clim, aes(x = tmax))+
+ggplot(filter(prov_clim, month %in% c(6)), aes(x = tmax))+
   geom_density(aes(color = spp), alpha = 0.4)+
-  geom_vline(xintercept = mean(filter(prov_clim, spp == "PIEN")$tmax), color = "red", linetype = 2)+
-  geom_vline(xintercept = mean(filter(prov_clim, spp == "PIFL")$tmax), color = "darkgreen", linetype = 2)+
-  geom_vline(xintercept = mean(filter(prov_clim, spp == "PIPO")$tmax), color = "turquoise", linetype = 2)+
-  geom_vline(xintercept = mean(filter(prov_clim, spp == "PSME")$tmax), color = "purple", linetype = 2)+
+  geom_vline(xintercept = median(filter(prov_clim, month == 6 & spp == "PIEN")$tmax), color = "red", linetype = 2)+
+  geom_vline(xintercept = median(filter(prov_clim, month == 6 & spp == "PIFL")$tmax), color = "green4", linetype = 2)+
+  geom_vline(xintercept = median(filter(prov_clim, month == 6 & spp == "PIPO")$tmax), color = "turquoise2", linetype = 2)+
+  geom_vline(xintercept = median(filter(prov_clim, month == 6 & spp == "PSME")$tmax), color = "purple", linetype = 2)+
   labs(x = "Max Daily Temp (ºC): 1984 - 2024",
        color = "Species")+
   theme_light(base_size = 20)
 
-ggplot(prov_clim, aes(y = tmax))+
+ggplot(prov_clim, aes(x = tmax))+
   geom_histogram(aes(fill = spp), alpha = 0.4, binwidth = 0.5)+
   #geom_vline(yintercept = mean(prov_clim$tmax), color = "red", linetype = 2)+
-  labs(y = "Max Daily Temp (ºC): 1984 - 2024",
+  labs(x = "Max Daily Temp (ºC): 1984 - 2024",
        fill = "Species")+
   theme_light(base_size = 20)
 
 mean(prov_clim$tmax)
-
-options(digits = 6)
-TukeyHSD(aov(tmax ~ spp, prov_clim))
 
 
 ggplot(avg_clim, aes(x = year, y = temp))+
