a1c3f2ee1d37a4dd119d9d9094459f3c9eb234c0
Fern Bromley <fernb@arizona.edu>
2025-08-11 18:26:05 -0700

    starting stomatal density/getting csvs

--- scripts/Conductance.R
+++ scripts/Conductance.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 water <- read_csv("data/Experiment/Raw/Watered_Plants.csv")$TreeID
+dates <- read_csv("data/Experiment/Dates.csv")
 
 con_files <- list.files("data/Experiment/Raw/Conductance", full.names = T)
 
@@ -19,19 +20,30 @@ con <- bind_rows(con_dat) %>%
   mutate(temp = case_when(id < 31 ~ "ambient",
                           id >= 31 ~ "heatwave"),
          water = case_when(TreeID %in% water ~ "water",
-                           .default = "drought"))
+                           .default = "drought")) %>%
+  inner_join(dates)
 
 # determine effective 0 conductance:
 # mean(c(84.4, 82, 80.1, 78.1)) # mean of values in mmol/m2s on Whatman Paper = 81.15 mmol/m2/s
 
 
-ggplot(data = con, aes(x = date, y = con, group = interaction(spp, water)))+
+ggplot(data = con, aes(x = date, y = con, group = interaction(spp, temp, water)))+
   # geom_boxplot(aes(group = interaction(date, water), fill = water))+
   # geom_line(aes(group = TreeID), alpha = 0.2)+
   geom_point(alpha = 0.7, aes(color = water))+
   geom_hline(yintercept = 81.15)+
   geom_smooth(method = "lm", aes(color = water, fill = water), se = T)+
-  facet_wrap(~interaction(spp), nrow = 4)+
+  facet_wrap(~interaction(spp, temp), nrow = 4)+
+  theme_light(base_size = 20)+
+  labs(x = "Date", y = "Foliar conductance (mmol/m2s)")
+
+ggplot(data = con, aes(x = week, y = log(con), group = interaction(spp, temp, water)))+
+  # geom_boxplot(aes(group = interaction(date, water), fill = water))+
+  # geom_line(aes(group = TreeID), alpha = 0.2)+
+  geom_boxplot(alpha = 0.7, aes(group = interaction(date, water), fill = water))+
+  # geom_hline(yintercept = 81.15)+
+  # geom_smooth(method = "lm", aes(color = water, fill = water), se = T)+
+  facet_wrap(~interaction(spp, temp), nrow = 4, scales = "free_y")+
   theme_light(base_size = 20)+
   labs(x = "Date", y = "Foliar conductance (mmol/m2s)")
 
