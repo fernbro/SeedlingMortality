a83454283ff63de0d5bca30df94445810e9e1f9f
Fern Bromley <fernb@arizona.edu>
2025-09-05 16:36:47 -0700

    week 7 done ish

--- scripts/Fluorescence.R
+++ scripts/Fluorescence.R
@@ -26,7 +26,8 @@ fluor <- bind_rows(fl_dat) %>%
   # group_by(Fv_Fm_dark, spp, water) %>% 
   # mutate(n_obs = n())
 
-write_csv(fluor, "data/Experiment/Processed/Fluorescence.csv")
+# write_csv(fluor, "data/Experiment/Processed/Fluorescence.csv")
+
 
 ggplot(fluor, aes(x = factor(week), y = Fv_Fm_dark, group = spp))+
   geom_line(alpha = 0.2, aes(group = TreeID, linetype = temp))+
@@ -35,11 +36,12 @@ ggplot(fluor, aes(x = factor(week), y = Fv_Fm_dark, group = spp))+
   # geom_hline(yintercept = 0.85)+
   facet_wrap(~interaction(spp))+
   scale_fill_manual(values = hw_colors)+
-  # geom_smooth(aes(group = interaction(water, temp), fill = temp))+
+  # geom_smooth(method = "lm", aes(group = interaction(water, temp), fill = temp))+
   # ylim(c(0, 0.9))+
   theme_light(base_size = 26)+
   labs(x = "Week", y = "Fv/Fm", shape = "Temp", linetype = "Temp",
        color = "Water")
+# ggsave("figures/FvFm090225.png", width = 10, height = 8, units = "in")
 
 ggplot(fluor, aes(x = date, y = Fv_Fm_dark, group = spp))+
   #geom_line(alpha = 0.4, aes(group = TreeID))+
@@ -63,3 +65,21 @@ ggplot(fluor, aes(x = water, y = Fv_Fm_dark))+
   theme_light(base_size = 26)+
   labs(x = "Species", y = "Fv/Fm", shape = "Number of obs")
 # ggsave("figures/FvFm080425.png", last_plot(), width = 10, height = 6)
+
+
+# averages for each spp and treatment over time:
+fl_avg <- fluor %>% 
+  group_by(spp, water, temp, week) %>% 
+  summarise(f_mean = mean(Fv_Fm_dark), f_sd = sd(Fv_Fm_dark))
+
+ggplot(filter(fl_avg), aes(x = week, y = f_mean, color = water))+
+  annotate("rect", alpha = 0.5, xmin = 3.5, xmax = 4.5, ymin = 0.72, ymax = 0.83,
+           fill = "gray80")+
+  geom_point(size = 3)+
+  geom_errorbar(aes(ymin = f_mean - f_sd, ymax = f_mean + f_sd),
+                width = 0.1, alpha = 0.7)+
+  facet_wrap(~interaction(temp, spp), ncol = 2)+
+  theme_light()+
+  theme(strip.background = element_rect(color = "black", fill = "white"))+
+  theme(strip.text = element_text(colour = 'black'))+
+  labs(x = "Week", y = "Fv/Fm")
