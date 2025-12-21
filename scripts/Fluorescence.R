278ca4898c783ba7d0362cbad87f830efcf0732c
Fern Bromley <fernb@arizona.edu>
2025-12-20 23:24:34 -0700

    oof

--- scripts/Fluorescence.R
+++ scripts/Fluorescence.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(dplyr)
 
 start_exp <- yday(as.POSIXct("2025-07-21"))
 water <- read_csv("data/Experiment/Raw/Watered_Plants.csv")$TreeID
@@ -20,7 +21,7 @@ fluor <- bind_rows(fl_dat) %>%
   mutate(date = date(as.POSIXct(textdate, tryFormats = "%m%d%Y")),
          spp = str_sub(TreeID, start = 1, end = 4),
          id = as.numeric(str_sub(TreeID, start = 5, end = 6))) %>% 
-  select(-textdate) %>% 
+  select(-textdate) %>%
   mutate(temp = case_when(id < 31 ~ "ambient",
                           id >= 31 ~ "heatwave"),
          water = case_when(TreeID %in% water ~ "water",
@@ -42,20 +43,44 @@ dead_trees <- (filter(fluor, Fv_Fm_dark < 0.1, water == "drought")) %>%
 # PLUS psme22 and pipo56 (removed before 2 weeks of fl<0.1 bc of mold)
 # all droughted FLUORESCED of PIEN, PIPO, and PSME are dead as of 11/30/2025
 
+fluor$spp <- factor(fluor$spp, levels = c("PSME", "PIPO", "PIEN", "PIFL"))
+
+
 ggplot(fluor, aes(x = day, y = Fv_Fm_dark, group = spp))+
   geom_hline(yintercept = 0.1, color = "red", linewidth = 1.5, alpha = 0.1)+
   geom_line(alpha = 0.3, aes(group = TreeID, linetype = temp))+
   geom_point(aes(group = interaction(date, water), fill = water, color = water,
                  shape = temp), 
              size = 2, alpha = 0.6)+
-  facet_wrap(~spp)+
-  annotate("rect", alpha = 0.3, xmin = hw_days[1], xmax = hw_days[2], ymin = 0, ymax = 0.85,
-           fill = "orange")+
+  facet_wrap(~spp, ncol = 1)+
+  # annotate("rect", alpha = 0.3, xmin = hw_days[1], xmax = hw_days[2], ymin = 0, ymax = 0.85,
+  #          fill = "orange")+
   # scale_fill_manual(values = hw_colors)+
   scale_shape_manual(values=c(21,22))+
-  theme_minimal(base_size = 26)+
+  theme_minimal(base_size = 15)+
   # geom_smooth(aes(group = interaction(water, temp), fill = temp, linetype = water))+
   # theme(strip.background = element_rect(color = "black", fill = "white"))+
   # theme(strip.text = element_text(colour = 'black'))+
   labs(x = "Day", y = "Fv/Fm", shape = "Temp", linetype = "Temp", color = "Water", fill = "Water")
 # ggsave("figures/FvFm090225.png", width = 10, height = 8, units = "in")
+
+
+# new aesthetics:
+ggplot(fluor, aes(x = day, y = Fv_Fm_dark, group = spp))+
+  geom_hline(yintercept = 0.1, color = "red", linewidth = 1.5, alpha = 0.1)+
+  geom_line(alpha = 0.3, aes(group = TreeID, linetype = temp, color = temp))+
+  geom_point(aes(shape = water,
+                 fill = temp,
+                 color = temp), 
+             # color = "black",
+             alpha = 0.7)+
+  facet_wrap(~spp+water, ncol = 2)+
+  scale_fill_manual(values = hw_colors, guide = T)+
+  scale_color_manual(values = hw_colors)+
+  scale_shape_manual(values=c(22, 1))+
+  theme_minimal(base_size = 20)+
+  guides(fill = guide_legend(override.aes = list(shape = c(22,21))))+
+  labs(x = "Day", y = "Fv/Fm", 
+       shape = "Water", 
+       linetype = "Temp",
+       fill = "Temp")
--- scripts/Mortality_LogRegression.R
+++ scripts/Mortality_LogRegression.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(emmeans)
+library(drc)
 
 start_exp <- yday(as.POSIXct("2025-07-21"))
 fl <- read_csv("data/Experiment/Processed/Fluorescence.csv") %>% 
@@ -43,6 +45,7 @@ tree_fl$spp <- factor(tree_fl$spp, levels = c("PSME", "PIPO", "PIEN", "PIFL"))
 ggplot(filter(tree_fl, water == "drought"), aes(x = day, y = life*100))+
   # geom_point(aes(color = temp, shape = temp), alpha = 0.3)+
   # scale_shape_manual(values = c(21,2))+
+  geom_hline(yintercept = 50, color = "gray50", linetype=4)+
   geom_line(aes(color = temp, linetype = temp,
                 group = interaction(temp, water),
     x = day, y = 100*pred))+
@@ -54,3 +57,28 @@ ggplot(filter(tree_fl, water == "drought"), aes(x = day, y = life*100))+
   facet_wrap(~spp, ncol = 1)+
   theme_minimal(base_size = 15)+
   labs(x = "Days of drought", y = "% survival")
+
+# which day does pred = 50 for each spp, treatment?
+# LD50 of days of drought:
+
+ED(drm(mort_glm))
+
+get_LD50 = function(fit){
+  data.frame(
+    LD50 = dose.p(fit)[1],
+    CI = attributes(dose.p(fit))$SE[,1]*qnorm(0.975)
+  )
+}
+
+ld50s <- tree_fl %>% 
+  filter(water == "drought") %>% 
+  group_by(temp, spp) %>% 
+  do(get_LD50(glm(life ~ day, family = "binomial", data = .)))
+
+ggplot(ld50s, aes(x = LD50, y = spp, color = temp))+
+  geom_errorbar(aes(xmin = LD50-CI, xmax = LD50+CI), width = 0.2)+
+  geom_point(size = 2)+
+  theme_minimal(base_size = 20)+
+  labs(x = "Drought days needed for 50% mortality",
+       y = "Species",
+       color = "Temp")
